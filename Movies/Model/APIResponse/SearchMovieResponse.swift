89fc1f53c7c3158d4907326621b0b4ce7f5efdb6
Emre Berk <emreberk856@gmail.com>
2017-12-11 23:05:40 +0300

    pods, views, models

---
+++ Movies/Model/APIResponse/SearchMovieResponse.swift
@@ -0,0 +1,9 @@
+//
+//  SearchMovieResponse.swift
+//  Movies
+//
+//  Created by Emre Berk on 10.12.2017.
+//  Copyright © 2017 Emre Berk. All rights reserved.
+//
+
+import Foundation
---
+++ Movies/View/Cells/SearchSuggestTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  SearchSuggestTableViewCell.swift
+//  Movies
+//
+//  Created by Emre Berk on 10.12.2017.
+//  Copyright © 2017 Emre Berk. All rights reserved.
+//
+
+import Foundation
